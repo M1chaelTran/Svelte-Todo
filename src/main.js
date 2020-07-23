d1004126f9bb34864ace34241da9aa3096b09791
Michael <michaeltran.online@gmail.com>
2020-07-23 23:36:19 +1000

    Allow fakedb to run in prod

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import App from './App.svelte'
 import '@material/typography/mdc-typography.scss'
 import fakerServer from 'app/fakedb'
 
-if (!__myapp.env.isProd) {
-  fakerServer()
-}
+fakerServer()
 
 const app = new App({ target: document.body })
 
